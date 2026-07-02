64e695e9d59dce390cabfd99742e07af3deac9b2
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-07-02 12:57:04 +0530

    feat: initialize Express application with authentication routes, request logging, and middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auht.routes.js';
+import { json, timestamp } from 'drizzle-orm/gel-core';
 
 const app = express();
 app.use(helmet());
@@ -29,5 +31,19 @@ app.get('/', (req, res) => {
     logger.info("hello from acquisitions!!");
     res.status(200).send('hello from Acquisitions');
 });
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime()
+    })
+})
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: "acqisitions api is running.." });
+})
+
+
+app.use('/api/auth', authRoutes); //api route
 
 export default app;
